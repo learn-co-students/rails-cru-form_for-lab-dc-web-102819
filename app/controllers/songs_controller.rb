2fa336810f54e976e728bbd884085a0377c5cabb
mrh9ud <mrh9ud@virginia.edu>
2019-11-23 19:03:02 -0500

    two failures to resolve

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,4 +15,14 @@ class SongsController < ApplicationController
     def show
         @song = Song.find(params[:id])
     end
+
+    def edit
+        @song = Song.find(params[:id])
+    end
+
+    def update
+        @song = Song.find(params[:id])
+        @song.update(params.require(:song).permit(:name, :artist_id, :genre_id))
+        redirect_to song_path(@song)
+    end
 end
